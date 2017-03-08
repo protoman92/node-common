7209f6bd22c95d064c53c2e10f614a2465ca8e28
Hai Pham <swiften.svc@gmail.com>
2017-03-08 10:44:30 +0800

    Added shuffle for arrays

--- public/test/util/collection.js
+++ public/test/util/collection.js
@@ -6,6 +6,17 @@ const
 
 utils.includeUtils();
 
+Array.prototype.shuffle = function() {
+    for (var i = this.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = this[i];
+        this[i] = this[j];
+        this[j] = temp;
+    }
+
+	return this;
+};
+
 describe("Function Tests", function() {
 	it(
 		"Array.last() function should work correctly",
