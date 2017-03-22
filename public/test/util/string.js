66c655ab007c5404d27c38c6469f6e038ebef5f2
Hai Pham <swiften.svc@gmail.com>
2017-03-22 23:01:33 +0800

    Changed randomString method name

--- public/test/util/string.js
+++ public/test/util/string.js
@@ -1,10 +1,13 @@
 // @flow
 
-String.randomString = function (length) {
+/**
+ * Create a random {@link String} based on the specified length.
+ * @return {String} A String object.
+ */
+String.random = function (length) {
   const all = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  return new Array(length)
-    .fill('')
+  return new Array(length).fill('')
     .map(() => Math.floor(Math.random() * all.length))
     .map(index => all.charAt(index))
     .join('');
